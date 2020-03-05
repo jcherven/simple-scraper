1a3138e59c17089d4539379b32b5962a9111d460
jcherven <cherven.james@gmail.com>
2020-03-05 16:23:25 -0500

    Adds a spoofed header to the main request

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const request = require('request-promise');
+const cheerio = require('cheerio');
 
 const URL = 'https://www.imdb.com/title/tt0102926/?ref_=fn_al_tt_1';
 
 (async () => {
-  const response = await request(URL);
+  const response = await request({
+    uri: URL,
+    headers: {
+      'Accept':
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+      'Accept-encoding': 'gzip, deflate, br',
+      'Accept-language': 'en-US,en;q=0.9',
+      'Cache-Control': 'max-age=0',
+      'Connection': 'keep-alive',
+      'Host': 'www.imdb.com',
+      'Upgrade-Insecure-Requests': '1',
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'
+    },
+    gzip: true
+  });
 
   let $ = cheerio.load(response);
 
